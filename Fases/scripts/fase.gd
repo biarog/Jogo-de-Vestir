269ce3acbb7436fa3ebb973ca8bc172f71b13535
biarog <66644264+biarog@users.noreply.github.com>
2024-12-07 16:29:52 -0300

    Correcoes Basicas no codigo da fase

--- Fases/scripts/fase.gd
+++ Fases/scripts/fase.gd
@@ -1,49 +1,52 @@
 extends Node2D
-var categoria := 0
-var player : Node2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	player = $Corpo_2
+@export var categoria_se := 0
+@export var categoria_fc := 0
+@export var pontuacao = 0
+@onready var player : Node2D
 
 func sistema_avaliacao_roupas():
-	var pontuacao = 0
 	# categorias:
-		#0: simples
-		#1: extravagante
-		#2: casual
-		#3: formal
-	if (categoria == 0): #simples
+		#categoria_se = 0: simples
+		#categoria_se = 1: extravagante
+		#categoria_fc = 0: formal
+		#categoria_fc = 1: casual
+	#Pontuacoes:
+		# 0 =< pont < 3 = ruim
+		# 3 =< pont < 6 = ok
+		# pont == 6     = pfto
+
+	if (categoria_se == 0): #simples
 		if ($Player.val_top == 0 || $Player.val_top == 1):
 			pontuacao = pontuacao+1
-		elif ($Player.val_bottom == 0 || $Player.val_bottom == 1):
+		if ($Player.val_bottom == 0 || $Player.val_bottom == 1):
 			pontuacao = pontuacao+1
-		elif ($Player.val_sapato == 0 || $Player.val_sapato == 1):
+		if ($Player.val_sapato == 0 || $Player.val_sapato == 1):
 			pontuacao = pontuacao+1
 			
-	elif (categoria == 1): #extravagante
+	elif (categoria_se == 1): #extravagante
 		if ($Player.val_top == 2 || $Player.val_top == 3):
 			pontuacao = pontuacao+1
-		elif ($Player.val_bottom == 2 || $Player.val_bottom == 3):
+		if ($Player.val_bottom == 2 || $Player.val_bottom == 3):
 			pontuacao = pontuacao+1
-		elif ($Player.val_sapato == 2 || $Player.val_sapato == 3):
+		if ($Player.val_sapato == 2 || $Player.val_sapato == 3):
 			pontuacao = pontuacao+1
 			
-	elif (categoria == 2): #casual
+	if (categoria_fc == 0): #casual
 		if ($Player.val_top == 1 || $Player.val_top == 3):
 			pontuacao = pontuacao+1
-		elif ($Player.val_bottom == 1 || $Player.val_bottom == 3):
+		if ($Player.val_bottom == 1 || $Player.val_bottom == 3):
 			pontuacao = pontuacao+1
-		elif ($Player.val_sapato == 1 || $Player.val_sapato == 3):
+		if ($Player.val_sapato == 1 || $Player.val_sapato == 3):
 			pontuacao = pontuacao+1
 			
-	elif (categoria == 3): #formal
+	elif (categoria_fc == 1): #formal
 		if ($Player.val_top == 0 || $Player.val_top == 2):
 			pontuacao = pontuacao+1
-		elif ($Player.val_bottom == 0 || $Player.val_bottom == 2):
+		if ($Player.val_bottom == 0 || $Player.val_bottom == 2):
 			pontuacao = pontuacao+1
-		elif ($Player.val_sapato == 0 || $Player.val_sapato == 2):
+		if ($Player.val_sapato == 0 || $Player.val_sapato == 2):
 			pontuacao = pontuacao+1
+
 
 func _on_menu_roupas_valor_atualizado() -> void:
 	player.set_cabelo($Menu_Roupas.val_cabelo)
