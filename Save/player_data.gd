72f75a56c5b6b34d1f220fcd7baf078c2fd54f5f
Matteo Savan <matteosavan@gmail.com>
2025-01-30 22:22:03 -0300

    playerdata atualizado

--- Save/player_data.gd
+++ Save/player_data.gd
@@ -3,6 +3,11 @@ class_name PlayerData
 
 static var corpo: int = 0 #contém valor 1 (magro) ou 2 (gordo) 
 static var cor: int = 0 #contém o valor de 0 (mais claro) a 7 (mais escuro)
+static var olho: int = 0
+static var sapato: int = 0
+static var top: int = 0
+static var bottom: int = 0
+static var cabelo: int = 0
 
 static func get_corpo() -> int:
 	return corpo
@@ -15,3 +20,33 @@ static func set_corpo(valor: int) -> void:
 	
 static func set_cor(valor: int) -> void:
 	cor = valor
+
+static func get_cabelo() -> int:
+	return cabelo
+
+static func set_cabelo(valor: int) -> void:
+	cabelo = valor
+
+static func get_olho() -> int:
+	return olho
+
+static func set_olho(valor: int) -> void:
+	olho = valor
+
+static func get_sapato() -> int:
+	return sapato
+
+static func set_sapato(valor: int) -> void:
+	sapato = valor
+
+static func get_top() -> int:
+	return top
+
+static func set_top(valor: int) -> void:
+	top = valor
+
+static func get_bottom() -> int:
+	return bottom
+
+static func set_bottom(valor: int) -> void:
+	bottom = valor
